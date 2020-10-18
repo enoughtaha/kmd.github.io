7a9227b3c1d1b1542665878e8ed8af73d0cdf479
enoughtaha <ahnaf@getcompound.com>
2020-10-18 01:27:22 -0400

    First Commit

---
+++ precache-manifest.e6844496f11d67dc9a6938fa469ae750.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0e04cf85c5a2a17f5f9f69546747d2",
+    "url": "/kmd.github.io/index.html"
+  },
+  {
+    "revision": "470573f43d0562b7af4f",
+    "url": "/kmd.github.io/static/css/2.87583f7f.chunk.css"
+  },
+  {
+    "revision": "686c17bf236ed5390ae5",
+    "url": "/kmd.github.io/static/css/main.fe6082eb.chunk.css"
+  },
+  {
+    "revision": "470573f43d0562b7af4f",
+    "url": "/kmd.github.io/static/js/2.47fc108b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/kmd.github.io/static/js/2.47fc108b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686c17bf236ed5390ae5",
+    "url": "/kmd.github.io/static/js/main.1a11263e.chunk.js"
+  },
+  {
+    "revision": "3590cfe765a6c3cc3230",
+    "url": "/kmd.github.io/static/js/runtime-main.8adc0255.js"
+  },
+  {
+    "revision": "63538255aa6c7c8c19907f6f32c7d50c",
+    "url": "/kmd.github.io/static/media/about-k.63538255.png"
+  },
+  {
+    "revision": "c51f17e6f6caec4c7ceae0340d9b1f2d",
+    "url": "/kmd.github.io/static/media/about.c51f17e6.png"
+  },
+  {
+    "revision": "af472e7163fa731173f7fea2dc38b068",
+    "url": "/kmd.github.io/static/media/contact-1.af472e71.png"
+  },
+  {
+    "revision": "2353af5b916076e15b0e4832971cabbe",
+    "url": "/kmd.github.io/static/media/door.2353af5b.png"
+  },
+  {
+    "revision": "0f1df5a73f38e6503abfe76344d79068",
+    "url": "/kmd.github.io/static/media/down-arrow.0f1df5a7.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kmd.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6d6b470806673f0f38ff2298356dcf3c",
+    "url": "/kmd.github.io/static/media/research.6d6b4708.png"
+  }
+]);
